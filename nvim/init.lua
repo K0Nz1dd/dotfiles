b403ba696b4a8f179977b949bbe9550431186971
K0Nz1dd <k0nz1dd@gmail.com>
2025-12-19 15:00:11 +0000

    Remove deprecated configuration files and scripts for Neovim, Waybar, and other utilities; clean up unused plugins and settings.

--- nvim/init.lua
+++
@@ -1,7 +0,0 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-
-vim.opt.encoding = 'utf-8'
-vim.opt.fileencoding = 'utf-8'
-vim.opt.fileencodings = 'utf-8,en'
-
--- scripts/obs-status.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ACTIVE_WINDOW_CLASS=$(hyprctl -j activewindow | jq -r '.class')
-
-if [[ "$ACTIVE_WINDOW_CLASS" == "obs" ]]; then
-    echo "󰒃 OBS"
-else
-    echo ""
-fi
